fae777f007bc5274df16a4fc862e3d63fc209ada
Sree Polavarapu <sree.polavarapu@spark.co.nz>
2021-02-28 18:24:19 +1300

    Req #puppies - list item wip

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,12 +18,22 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.typography
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +50,36 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        List(puppies.size) { index ->
+            val puppy = puppies[index]
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = puppy.imageResId),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .height(90.dp)
+                        .width(120.dp)
+                        .clip(RoundedCornerShape(4.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Column(modifier = Modifier
+                    .padding(start = 16.dp)
+                    .fillMaxWidth()
+                ) {
+                    Text(text = puppy.name, style = typography.h6)
+                    Text(text = puppy.breed, style = typography.body1)
+                    Text(
+                        text = puppy.sex + " . " + puppy.age + " . " + puppy.distance,
+                        style = typography.caption
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -59,3 +98,25 @@ fun DarkPreview() {
         MyApp()
     }
 }
+
+data class Puppy(
+    val name: String,
+    val age: String,
+    val breed: String,
+    val imageResId: Int,
+    val distance: String,
+    val favourite: Boolean,
+    val sex: String
+)
+
+private val puppies = listOf(
+    Puppy(
+        "LULU",
+        age = "4 months",
+        breed = "Pit Bull",
+        distance = "2 miles",
+        favourite = false,
+        sex = "Male",
+        imageResId = R.drawable.pitbull_baby
+    )
+)
